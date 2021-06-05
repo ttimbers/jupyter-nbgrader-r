13875d30ea226bc9398fa342e28750736576225e
Tiffany Timbers <tiffany.timbers@gmail.com>
2021-06-04 21:02:50 -0700

    Made hints more helpful
    Closes #1

--- submitted/hacker/worksheet_01_r/tests_worksheet_01.R
+++ submitted/hacker/worksheet_01_r/tests_worksheet_01.R
@@ -11,15 +11,22 @@ test_2.1 <- function(){
     test_that('Solution is incorrect, the readr package needs to be loaded. Try doing this with the library function.', {
         expect_true("package:readr" %in% search())
         })
-print("Success!")
-    }
+    print("Success!")
+}
 
 test_2.2 <- function(){
+    if (digest(toupper(answer2.2)) == "75f1160e72554f4270c809f041c7a776") {
+        print("Not quite! We are trying to visualize the relationship between two quantitative variables, bar charts are not the most effective visualization for this.")
+    } else if (digest(toupper(answer2.2)) == "3a5505c06543876fe45598b5e5e5195d") {
+        print("Not quite! We are trying to visualize the relationship between two quantitative variables, pie charts are not the most effective visualization for this.")
+    } else if (digest(toupper(answer2.2)) == "c1f86f7430df7ddb256980ea6a3b57a4") {
+        print("Not quite! We are trying to visualize the relationship between two quantitative variables, box plots are not the most effective visualization for this.")
+    }
     test_that('Solution is incorrect', {
         expect_equal(digest(toupper(answer2.2)), '475bf9280aab63a82af60791302736f6') # we hid the answer to the test here so you can't see it, but we can still run the test
         })
-print("Success!")
-    }
+    print("Success!")
+}
 
 test_2.3 <- function(){
     test_that('Did not create an object named race_times', {
@@ -28,29 +35,29 @@ test_2.3 <- function(){
     test_that('race_times should be a data frame', {
         expect_true('data.frame' %in% class(race_times))
         })
-    test_that('race_times does not contain the correct data', {
+    test_that('race_times does not contain the correct data.', {
         expect_equal(dim(race_times), c(1833, 5))
         expect_equal(sum(race_times$age), 66455.5)
         expect_equal(colnames(race_times), c("age", "bmi", "km5_time_seconds", "km10_time_seconds", "sex"))
         })
-print("Success!")
+    print("Success!")
     }
 
 test_2.4 <- function(){
-    test_that('race_times has the incorrect number of rows', {
+    test_that('race_times_women has the incorrect number of rows, did you remember to filter for females?', {
         expect_equal(digest(nrow(race_times_women)), '22c7b9e96a1f1a8c4a13dc8b6586dc80') # we hid the answer to the test here so you can't see it, but we can still run the test
         })
-    test_that('race_times has the incorrect number of columns', {
+    test_that('race_times_women has the incorrect number of columns', {
         expect_equal(digest(ncol(race_times_women)), 'dd4ad37ee474732a009111e3456e7ed7') # we hid the answer to the test here so you can't see it, but we can still run the test   
         })
-    test_that('race_times has the wrong columns', {
+    test_that('race_times_women has the wrong column names', {
         expect_equal(digest(paste0(sort(colnames(race_times_women)), collapse = "")), 'e40c810969d18f6fff2c8a290c0ffb37') # we hid the answer to the test here so you can't see it, but we can still run the test   
         })
-    test_that('marathon_filtered bmi column contains the incorrect values', {
+    test_that('race_times_women bmi column contains the incorrect values, did you remember to filter for females?', {
         expect_equal(digest(as.numeric(sum(race_times_women$bmi))), '7cc4baefd16add414fe6a9e051a2f5f5') # we hid the answer to the test here so you can't see it, but we can still run the test
         })
-print("Success!")
-    }
+    print("Success!")
+}
 
 test_2.5 <- function(){
     test_that('Did not create a plot named race_times_plot', {
@@ -70,11 +77,16 @@ test_2.5 <- function(){
         expect_false(race_times_plot$labels$y == 'km5_time_seconds')
         })
     print("Success!")
-    }
+}
 
 test_2.6 <- function(){
+    if (digest(toupper(answer2.6)) == "75f1160e72554f4270c809f041c7a776") {
+        print("Not quite! Look at how as the values for BMI increase, so do the values for race time.")
+    } else if (digest(toupper(answer2.6)) == "475bf9280aab63a82af60791302736f6") {
+        print("Close, but not quite! You are right that there is a relationship, but check the direction.")
+    }
     test_that('Solution is incorrect', {
         expect_match(digest(toupper(answer2.6)), '3a5505c06543876fe45598b5e5e5195d')
         })
-print("Success!")
-    }
+    print("Success!")
+}
